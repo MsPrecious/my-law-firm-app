caa688425dcb1af690da3fcd38768f7700613e16
Anmol <pandeanmol26@gmail.com>
2023-09-09 01:08:20 +0530

    commited

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,10 +6,10 @@ const HeroSection = () => {
     <div className="hero-container">
       <img src={require('../images/lawfirm_logo.png')} alt="Law Firm Logo" className="logo" />
       <div className="nav-buttons">
-        <a href="#" className="nav-link">Home</a>
-        <a href="#" className="nav-link">Attorneys</a>
-        <a href="#" className="nav-link">Practice Areas</a>
-        <a href="#" className="nav-link">About us</a>
+        <a href="src/components/IntroSection.js" className="nav-link">Home</a>
+        <a href="src/components/IntroSection.js" className="nav-link">Attorneys</a>
+        <a href="src/components/IntroSection.js" className="nav-link">Practice Areas</a>
+        <a href="src/components/IntroSection.js" className="nav-link">About us</a>
       </div>
       <button className="contact-button">Contact Now</button>
       
